1c4bbe436be45c9bc310176ab2f05142135a19a3
Nikita <nikburinsky@mail.ru>
2025-08-14 16:06:23 +0300

    Created maui projects and solution

---
+++ src/Presentation/HabitTracker.Presentation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HabitTracker.Presentation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Presentation/HabitTracker.Presentation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTracker.Presentation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
