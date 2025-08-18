33803842bae86ea5ea46529d9267c0587d6ac665
JFomit <tzaberejny@gmail.com>
2025-08-18 21:17:38 +0300

    refactor: moved types to different files

---
+++ src/Domain/HabitTracker.Domain/GoodnessKind.cs
@@ -0,0 +1,7 @@
+namespace HabitTracker.Domain;
+
+enum GoodnessKind
+{
+    Positive = 0,
+    Negative,
+}
---
+++ src/Domain/HabitTracker.Domain/Icon.cs
@@ -0,0 +1,10 @@
+namespace HabitTracker.Domain;
+
+enum Icon
+{
+    Default = 0,
+
+    Training,
+    Running,
+    DrinkingWater
+}
