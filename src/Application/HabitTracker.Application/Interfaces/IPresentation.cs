08c93127dc26306716c6eb9cc33d1528c259b178
UterSt <kilbas.svetlana1982@gmail.com>
2025-08-19 17:40:16 +0300

    feat: create IPresentation interface

---
+++ src/Application/HabitTracker.Application/Interfaces/IPresentation.cs
@@ -0,0 +1,13 @@
+using HabitTracker.Domain.Dto;
+using JFomit.Functional;
+using JFomit.Functional.Monads;
+
+namespace HabitTracker.Application.Interfaces;
+
+public interface IPresentation
+{
+    public Result<Habit, string> CreateHabit(Habit habit);
+    public Result<Unit, string> UpdateHabit(Habit habit);
+    public Result<Unit, string> DeleteHabit(int id);
+    public Result<ICollection<Habit>, string> GetAllHabits();
+}
