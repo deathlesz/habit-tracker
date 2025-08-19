98f523452f69cfeb59248a2b957c141566ad8d05
Nikita <nikburinsky@mail.ru>
2025-08-19 12:07:28 +0300

    Reapply "Updating domain classes"
    This reverts commit eff73141defbbb1f5c6d9a7e00dd4dd580ffc4ea.

--- src/Domain/HabitTracker.Domain/Entities/Regularity/HabitScheduleEntity.cs
+++ src/Domain/HabitTracker.Domain/Entities/Regularity/HabitScheduleEntity.cs
@@ -8,47 +8,51 @@ using System.Threading.Tasks;
 namespace HabitTracker.Domain.Entities.Regularity
 {
     /// <summary>
-    /// Сущность содержащая информацию о РАСПИСАНИИ привычки. 
+    /// An entity that contains information about a habit's SCHEDULE.
     /// </summary>
     public class HabitScheduleEntity
     {
         /// <summary>
-        /// Запись длины цикла повторяющейся привычки. 
-        /// Даже если привычка повторяется 1 раз (прим. 1 неделю), рассматривать ее стоит также (1 повторение)
+        /// Will be automatically set by repository
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Record the length of a repeating habit cycle. 
+        /// Even if a habit is repeated 1 time (approx. 1 week), it should also be considered (1 repetition)
         /// </summary>
         public int RepeatingCycleDays { get; set; } // Для еженедельных целей - 7, ежедневных - 1...
         /// <summary>
-        /// Метка о том что привычку можно выполнять в любой день.
-        /// Тогда поля RepeatingDatesToMatch и AddictionalDatesToMatch не требуются
+        /// A label indicating that the habit can be performed on any day.
+        /// In this case, the RepeatingDatesToMatch and AddictionalDatesToMatch fields are not required
         /// </summary>
         public bool IsAnyDay { get; set; }
         /// <summary>
-        /// Метка о том что привычку можно выполнять в любой из дней в расписании (а не во все).
-        /// Логически, взаимоисключающие с IsAnyDay.
+        /// A label indicating that the habit can be performed on any day in the schedule (not on all days).
+        /// Logically, it is mutually exclusive with IsAnyDay.
         /// </summary>
         public bool IsAllMachedDays { get; set; }
         /// <summary>
-        /// Количество выполненных дней в цикле для выполнения таски.
+        /// The number of completed days in the cycle for completing a task.
         /// </summary>
-        public int CycleMachedDaysGoal { get; set; } 
+        public int CycleMachedDaysGoal { get; set; }
 
         /// <summary>
-        /// Начало отсчета цикла. Отсчет дней и циклов в привычке идет от этого дня. 
-        /// Пример - привычка, создана в среду с записью на повторение в пн, вт, чт.
-        /// Тогда StartDate - Понедельник той же недели, а в RepeatingDatesToMatch - 0, 1, 3
+        /// The start of the cycle. The count of days and cycles in the habit starts from this day. 
+        /// Example - a habit created on Wednesday with a repeat record on Monday, Tuesday, and Thursday.
+        /// In this case, StartDate is Monday of the same week, and RepeatingDatesToMatch is 0, 1, and 3
         /// </summary>
         public DateOnly? StartDate { get; set; }
         /// <summary>
-        /// При повторяющейся привычке, какие дни от начала должны повторятся на каждом цикле
+        /// In a recurring habit, which days from the beginning should be repeated in each cycle
         /// </summary>
         public ICollection<int> RepeatingDatesToMatch { get; set; }
 
         /// <summary>
-        /// Поле для хранения дней, в которые привычка была успешно отмечена
+        /// A field for storing the days when the habit was successfully marked
         /// </summary>
         public ICollection<DateOnly> DatesMatched { get; set; }
         /// <summary>
-        /// Количество отмеченных дней за цикл. 
+        /// The number of marked days per cycle.
         /// </summary>
         public int DaysMachedInCycle { get; set; } 
     }
