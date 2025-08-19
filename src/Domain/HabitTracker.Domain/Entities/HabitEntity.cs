33cd74e6afe3a6592469c6e25247bd03a0e37678
Nikita <nikburinsky@mail.ru>
2025-08-19 12:07:28 +0300

    Reapply "Updating domain classes"
    This reverts commit eff73141defbbb1f5c6d9a7e00dd4dd580ffc4ea.

--- src/Domain/HabitTracker.Domain/Entities/HabitEntity.cs
+++ src/Domain/HabitTracker.Domain/Entities/HabitEntity.cs
@@ -4,34 +4,34 @@ using HabitTracker.Domain.Enums;
 namespace HabitTracker.Domain.Entities
 {
     public record GoalInfo(string Name, MeasurementUnit Unit);
-    public record ReminderInfo(TimeOnly Time, string Message);
 
     public class HabitEntity
     {
+        public int Id { get; set; }
         /// <summary>
-        /// Название привычки
+        /// Discriminator of habits regularity type
+        /// </summary>
+        public HabitRegularityType HabitRegularityType { get; set; }
+        /// <summary>
+        /// Name of Habit
         /// </summary>
         public required string Name { get; init; }
-        /// <summary>
-        /// Текстовое описание привычки
-        /// </summary>
         public string Description { get; init; }
         public required GoalInfo Goal { get; init; }
         /// <summary>
-        /// Информация об уведомлении
+        /// Info about notification
         /// </summary>
-        public ReminderInfo Reminder { get; init; }
+        public HabitReminderEntity Reminder { get; init; }
         /// <summary>
-        /// Информация о расписании привычки и его выполнении
+        /// Info about schedule and its completing stage
         /// </summary>
         public required HabitScheduleEntity Regularity { get; init; }
         public required GoodnessKind Kind { get; init; }
         public required Icon Icon { get; init; }
         public required Color Color { get; init; }
         public PartOfTheDay? PartOfTheDay { get; init; }
-        public State? State { get; set; } =  (State?)Enums.State.Incomplete;
+        public State State { get; set; } = State.Incomplete;
         public DateOnly? StartDate { get; init; }
         public DateOnly? EndDate { get; init; }
-        
     }
 }
