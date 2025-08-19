b9577aad4a6c36324c731e8d03bd2a9d0d123fb3
Margit-The-Fell-Omen <wartim9494@gmail.com>
2025-08-19 22:05:08 +0300

    fix: 2 first buttons fixed

--- src/Presentation/HabitTracker.Presentation/AddPage.xaml.cs
+++ src/Presentation/HabitTracker.Presentation/AddPage.xaml.cs
@@ -17,41 +17,38 @@ public partial class AddPage : ContentPage
     // TODO: pack the entered data in struct (or what suits here best) and save in into DB
     private async void OnCancelClicked(object sender, EventArgs e) // pop-up window & return to home page
     {
-        // if (BindingContext is AddPageViewModel vm)
-        //     await vm.CancelCommand.ExecuteAsync(null);
+
     }
 
     private async void OnSaveClicked(object sender, EventArgs e)
     {
-        // TODO: 
+
     }
 
-    //private async void OnTypeClicked(object sender, EventArgs e)
-    //{
-    //    if (BindingContext is AddPageViewModel vm)
-    //        await vm.SelectHabitTypeCommand.ExecuteAsync(null);
-    //}
+    private async void OnTypeClicked(object sender, EventArgs e)
+    {
+
+    }
 
     private void OnHabitNameChanged(object sender, TextChangedEventArgs e)
     {
-        if (BindingContext is AddPageViewModel vm)
-            vm.HabitName = "Name";
+
     }
 
     private async void OnGoalClicked(object sender, EventArgs e)
     {
-        // TODO: 
+    
     }
     private async void OnStartDateClicked(object sender, EventArgs e)
     {
-        // TODO: 
+       
     }
     private async void OnEndDateClicked(object sender, EventArgs e)
     {
-        // TODO: 
+    
     }
     private async void OnReminderClicked(object sender, EventArgs e)
     {
-        // TODO: 
+    
     }
 }
