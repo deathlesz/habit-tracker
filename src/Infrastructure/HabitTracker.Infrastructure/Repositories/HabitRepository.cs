6d46b74924273f48d77ffdf4b4815ca6e816732b
Nikita Burinsky <123398594+NikitaBurinsky@users.noreply.github.com>
2025-08-21 20:25:48 +0300

    Simplifying result update
    Co-authored-by: Timofey Zaberejny <90050014+JFomit@users.noreply.github.com>

--- src/Infrastructure/HabitTracker.Infrastructure/Repositories/HabitRepository.cs
+++ src/Infrastructure/HabitTracker.Infrastructure/Repositories/HabitRepository.cs
@@ -66,7 +66,7 @@ namespace HabitTracker.Infrastructure.Platforms.Android.Repositories
 			}
 			catch (Exception ex)
 			{
-				return Result<HabitEntity, string>.Fail(ex.Message);
+				return Error(ex.Message);
 			}
 		}
 	}
