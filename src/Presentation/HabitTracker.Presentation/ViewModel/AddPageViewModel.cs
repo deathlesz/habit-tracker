583590fe0620650598e56d11e4733a86b22304c5
Margit-The-Fell-Omen <wartim9494@gmail.com>
2025-08-21 17:48:02 +0300

    feat: added RegularityPage

--- src/Presentation/HabitTracker.Presentation/ViewModel/AddPageViewModel.cs
+++ src/Presentation/HabitTracker.Presentation/ViewModel/AddPageViewModel.cs
@@ -7,6 +7,7 @@ using Microsoft.Maui.Graphics;
 
 namespace HabitTracker.Presentation.ViewModel;
 
+
 public partial class AddPageViewModel
 {
     public ColorChangingElement HabitTypeButton { get; init; }
@@ -32,7 +33,7 @@ public partial class AddPageViewModel
         };
         HabitNameEntry = new(ElementColorStyle.Default);
         HabitGoalEntry = new(ElementColorStyle.Default);
-        HabitGoalMUnitButton = new(ElementColorStyle.Default, "Select your habit goal measurement unit")
+        HabitGoalMUnitButton = new(ElementColorStyle.Default, "Select measurement unit")
         {
             Command = new Command(async () => await SelectGoalMUnitAsync())
         };
@@ -52,14 +53,8 @@ public partial class AddPageViewModel
         {
             Command = new Command(async () => await SelectReminderAsync())
         };
-        HabitStartDatePicker = new(ElementColorStyle.Default)
-        {
-            Command = new Command(async () => await SelectStartDateAsync())
-        };
-        HabitEndDatePicker = new(ElementColorStyle.Default)
-        {
-            Command = new Command(async () => await SelectEndDateAsync())
-        };
+        HabitStartDatePicker = new(ElementColorStyle.Default);
+        HabitEndDatePicker = new(ElementColorStyle.Default);
         HabitDescriptionEditor = new(ElementColorStyle.Default);
 
         // SelectIconColorCommand = new Command(async () => await SelectIconColorAsync());
@@ -68,6 +63,7 @@ public partial class AddPageViewModel
         CancelCommand = new Command(async () => await CancelAsync());
     }
 
+    // methods that display action sheets and alerts
     async Task SelectHabitTypeAsync()
     {
         var action = await Shell.Current.DisplayActionSheet(
@@ -89,7 +85,7 @@ public partial class AddPageViewModel
     private async Task SelectGoalMUnitAsync()
     {
         var action = await Shell.Current.DisplayActionSheet(
-            "Choose your goal", "Cancel", null, "Km", "Sec", "Count", "Step");
+            "Choose your goal measurement unit", "Cancel", null, "Km", "Sec", "Count", "Step", "M", "Min", "Hour", "Ml", "Cal", "G", "Mg", "Drink");
 
         if (string.IsNullOrEmpty(action) || action == "Cancel")
         {
@@ -99,7 +95,7 @@ public partial class AddPageViewModel
         HabitGoalMUnitButton.SetValue("Measurement unit:", action);
     }
 
-    private async Task SelectRegularityAsync() => await Shell.Current.DisplayAlert("Regularity", "Open regularity selector.", "OK");
+    private async Task SelectRegularityAsync() => await Shell.Current.GoToAsync($"{nameof(RegularityPage)}");
     private async Task SelectIconColorAsync() => await Shell.Current.DisplayAlert("Icon & Color", "Open icon/color picker.", "OK");
     private async Task SelectTimeOfDayAsync()
     {
@@ -120,12 +116,10 @@ public partial class AddPageViewModel
     }
 
     private async Task SelectReminderAsync() => await Shell.Current.DisplayAlert("Reminder", "Open reminder window.", "Ok");
-    private async Task SelectStartDateAsync() => await Shell.Current.DisplayAlert("Start date", "Open time picker.", "OK");
-    private async Task SelectEndDateAsync() => await Shell.Current.DisplayAlert("End date", "Open time picker.", "OK");
 
     private async Task SaveAsync()
     {
-        await Shell.Current.DisplayAlert("Saved", "Your habit has been saved.", "OK");
+        await Shell.Current.DisplayAlert("Saved", "Your habit has been saved.", "OK"); // TODO: implement habit saving
         await Shell.Current.GoToAsync("..");
     }
     private async Task CancelAsync() => await Shell.Current.GoToAsync("..");
