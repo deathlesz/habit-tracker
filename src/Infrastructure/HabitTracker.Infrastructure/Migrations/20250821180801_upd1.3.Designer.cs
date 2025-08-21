54c1a82611562f6177d2e61fd27eba2a719ada3c
Nikita Burinsky <nikburinsky@mail.ru>
2025-08-21 21:13:46 +0300

    Regenerated Migrations

--- src/Infrastructure/HabitTracker.Infrastructure/Migrations/20250821180801_upd1.3.Designer.cs
+++ src/Infrastructure/HabitTracker.Infrastructure/Migrations/20250821180801_upd1.3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HabitTracker.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HabitTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250821180801_upd1.3")]
+    partial class upd13
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.19");
---
+++ src/Infrastructure/HabitTracker.Infrastructure/Migrations/20250821180801_upd1.3.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class upd13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysMachedInCycle",
+                table: "HabitSchedules",
+                newName: "DaysMatchedInCycle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Habits",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaysMatchedInCycle",
+                table: "HabitSchedules",
+                newName: "DaysMachedInCycle");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Habits",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
