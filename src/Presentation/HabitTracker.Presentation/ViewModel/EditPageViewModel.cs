0c2c1fe782a45e2bdb8bf39f02e326404171da17
Deathlesz <deathless.mcd@gmail.com>
2025-08-24 16:03:00 +0300

    feat: remaining properties

--- src/Presentation/HabitTracker.Presentation/ViewModel/EditPageViewModel.cs
+++ src/Presentation/HabitTracker.Presentation/ViewModel/EditPageViewModel.cs
@@ -1,14 +1,13 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
-using Microsoft.Maui.Graphics;
 using HabitTracker.Domain.Dto;
-using JFomit.Functional;
+using JFomit.Functional.Extensions;
+using static JFomit.Functional.Prelude;
 
 namespace HabitTracker.Presentation.ViewModel;
+
 public partial class EditPageViewModel
 {
     public ColorChangingElement HabitTypeButton { get; init; }
@@ -29,13 +28,13 @@ public partial class EditPageViewModel
 
     public EditPageViewModel(Habit habit)
     {
-        HabitTypeButton = new(ElementColorStyle.Default, $"Habit type: {habit.Kind}", Prelude.Some(habit.Kind.ToString()))
+        HabitTypeButton = new(ElementColorStyle.Default, $"Habit type: {habit.Kind}", Some(habit.Kind.ToString()))
         {
             Command = new Command(async () => await SelectHabitTypeAsync()),
         };
-        HabitNameEntry = new(ElementColorStyle.Default, habit.Name, Prelude.Some(habit.Name));
-        HabitGoalEntry = new(ElementColorStyle.Default, habit.Goal.Name, Prelude.Some(habit.Goal.Name));
-        HabitGoalMUnitButton = new(ElementColorStyle.Default, $"Measurement unit: {habit.Goal.Unit}", Prelude.Some(habit.Goal.Unit.ToString()))
+        HabitNameEntry = new(ElementColorStyle.Default, habit.Name, Some(habit.Name));
+        HabitGoalEntry = new(ElementColorStyle.Default, habit.Goal.Name, Some(habit.Goal.Name));
+        HabitGoalMUnitButton = new(ElementColorStyle.Default, $"Measurement unit: {habit.Goal.Unit}", Some(habit.Goal.Unit.ToString()))
         {
             Command = new Command(async () => await SelectGoalMUnitAsync())
         };
@@ -43,25 +42,29 @@ public partial class EditPageViewModel
         {
             Command = new Command(async () => await SelectRegularityAsync())
         };
-        HabitIconButton = new(ElementColorStyle.Default, $"Icon: {habit.Icon}", Prelude.Some(habit.Icon.ToString()))
+        HabitIconButton = new(ElementColorStyle.Default, $"Icon: {habit.Icon}", Some(habit.Icon.ToString()))
         {
             Command = new Command(async () => await SelectIconAsync())
         };
-        HabitColorButton = new(ElementColorStyle.Default, "Choose the color from the list:")
+        // from here
+        HabitColorButton = new(ElementColorStyle.Default, $"Color: {habit.Color}", Some(habit.Color.ToString()))
         {
             Command = new Command(async () => await SelectColorAsync())
         };
-        HabitTimeOfDayButton = new(ElementColorStyle.Default, "Select time of day")
+        HabitTimeOfDayButton = new(ElementColorStyle.Default, $"Time of day: {habit.PartOfTheDay}", habit.PartOfTheDay.ToOption().Select(partOfTheDay => partOfTheDay.ToString()))
         {
             Command = new Command(async () => await SelectTimeOfDayAsync())
         };
-        HabitReminderButton = new(ElementColorStyle.Default)
+        HabitReminderButton = new(ElementColorStyle.Default, habit.Reminder.ToOption().Match(_ => "Edit reminder", () => "Add reminder"))
         {
             Command = new Command(async () => await SelectReminderAsync())
         };
-        HabitStartDatePicker = new(ElementColorStyle.Default);
-        HabitEndDatePicker = new(ElementColorStyle.Default);
-        HabitDescriptionEditor = new(ElementColorStyle.Default);
+        var startDate = habit.StartDate.ToOption().Select(date => date.ToString());
+        HabitStartDatePicker = new(ElementColorStyle.Default, $"{startDate}", startDate);
+
+        var endDate = habit.EndDate.ToOption().Select(date => date.ToString());
+        HabitEndDatePicker = new(ElementColorStyle.Default, $"{endDate}", endDate);
+        HabitDescriptionEditor = new(ElementColorStyle.Default, $"{habit.Description ?? ""}", Some(habit.Description ?? ""));
         SaveCommand = new Command(async () => await SaveAsync());
         CancelCommand = new Command(async () => await CancelAsync());
     }
