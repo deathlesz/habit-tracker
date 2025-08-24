74580775d9b09d24e6a469d70202ebe39243ff87
Deathlesz <deathless.mcd@gmail.com>
2025-08-24 20:57:31 +0300

    chore: return main page

--- src/Presentation/HabitTracker.Presentation/MainPage.xaml.cs
+++ src/Presentation/HabitTracker.Presentation/MainPage.xaml.cs
@@ -1,32 +1,16 @@
-using HabitTracker.Application.Interfaces.Repositories;
-
 namespace HabitTracker.Presentation
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        IHabitRepository habitRepository;
-        IHabitReminderRepository reminderRepository;
 
-        public MainPage(IHabitReminderRepository rRep, IHabitRepository hRep)
+        public MainPage()
         {
             InitializeComponent();
-            reminderRepository = rRep;
-            habitRepository = hRep;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnAddClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-            habitRepository.GetAll();
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushModalAsync(new AddPage());
         }
     }
-
 }
